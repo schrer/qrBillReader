ff25810145852ef06ee22b140b6f8b4ad2744118
Martin Schröder <ma.schroeder@outlook.com>
2022-02-14 14:40:34 +0100

    Code formatting in BillsAccordion

--- src/components/BillsAccordion.js
+++ src/components/BillsAccordion.js
@@ -2,29 +2,29 @@ import React from 'react';
 import { CAccordion, CAccordionItem, CAccordionHeader, CAccordionBody } from '@coreui/react';
 import BillTable from './BillTable';
 import { readR1Bills } from '../util/storage';
-import {useLiveQuery} from 'dexie-react-hooks';
+import { useLiveQuery } from 'dexie-react-hooks';
 
 
 const BillsAccordion = () => {
 
-  const qrBills = useLiveQuery( readR1Bills )
-  return (
-    <CAccordion activeItemKey={1}>
+    const qrBills = useLiveQuery(readR1Bills)
+    return (
+        <CAccordion activeItemKey={1}>
 
-      {
-        qrBills?.map( bill =>
-          <CAccordionItem itemKey={bill.id} key={bill.id}>
-            <CAccordionHeader>
-              {bill.billNumber}
-            </CAccordionHeader>
-            <CAccordionBody>
-              <BillTable bill={bill}/>
-            </CAccordionBody>
-          </CAccordionItem>
-        )
-      }
-    </CAccordion>
-  )
+            {
+                qrBills?.map(bill =>
+                    <CAccordionItem itemKey={bill.id} key={bill.id}>
+                        <CAccordionHeader>
+                            {bill.billNumber}
+                        </CAccordionHeader>
+                        <CAccordionBody>
+                            <BillTable bill={bill} />
+                        </CAccordionBody>
+                    </CAccordionItem>
+                )
+            }
+        </CAccordion>
+    )
 }
 
 export default React.memo(BillsAccordion)
