d1e25f61e4f0adfa3947511d089b17de4ae5df58
Martin Schröder <ma.schroeder@outlook.com>
2022-02-17 14:51:18 +0100

    Add imprint content

--- src/components/ImprintContent.js
+++ src/components/ImprintContent.js
@@ -1,16 +1,21 @@
 import React, { Suspense } from 'react';
-import { CContainer, CRow, CSpinner } from '@coreui/react';
+import { CCol, CContainer, CRow, CSpinner } from '@coreui/react';
+import { useTranslation } from 'react-i18next';
 
 const ImprintContent = () => {
-  return (
-    <CContainer lg>
-      <Suspense fallback={<CSpinner color="primary" />}>
-        <CRow>
-          
-        </CRow>
-      </Suspense>
-    </CContainer>
-  )
+    const translate = useTranslation().t;
+
+    return (
+        <CContainer lg>
+            <Suspense fallback={<CSpinner color="primary" />}>
+                <CRow>
+                    <CCol>
+                        <div dangerouslySetInnerHTML={{__html: translate('imprint.full', { 'interpolation': {'escapeValue': false} })}}/>
+                    </CCol>
+                </CRow>
+            </Suspense>
+        </CContainer>
+    )
 };
 
 export default React.memo(ImprintContent);
