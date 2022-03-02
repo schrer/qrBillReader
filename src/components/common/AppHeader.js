a27b1edda0a5977f11e9f4bde9c7e4d2753026f1
Martin Schröder <ma.schroeder@outlook.com>
2022-03-02 11:26:09 +0100

    Create dropdown in navbar for smaller screens

--- src/components/common/AppHeader.js
+++ src/components/common/AppHeader.js
@@ -3,32 +3,47 @@ import { NavLink } from 'react-router-dom';
 import {
     CContainer,
     CHeader,
-    CHeaderBrand,
     CHeaderNav,
     CNavLink,
-    CNavItem
+    CNavItem,
+    CDropdownToggle,
+    CDropdown,
+    CDropdownMenu,
+    CDropdownItem
 } from '@coreui/react';
 import { useTranslation } from 'react-i18next';
 import DeleteAllBillsModal from '../billspage/DeleteAllBillsModal';
+import {useLocation} from 'react-router-dom';
 
 
 const AppHeader = (props) => {
     const translate = useTranslation().t;
+    const location = useLocation();
+    //console.log(location)
 
+    // <CHeaderNav className="d-none d-md-flex me-auto">
     return (
         <CHeader position="sticky" className="mb-4">
             <CContainer fluid>
-                <CHeaderBrand className="mx-auto d-md-none" to="/">Header</CHeaderBrand>
-                <CHeaderNav className="d-none d-md-flex me-auto">
-                    <CNavItem>
+                <CHeaderNav>
+                    <CNavItem className='d-none d-lg-block'>
                         <CNavLink to="/" component={NavLink} activeclassname="active">
                             {translate('header.home')}
                         </CNavLink>
                     </CNavItem>
-                    <CNavItem>
+                    <CNavItem className='d-none d-lg-block'>
                         <CNavLink to="/about" component={NavLink}>
                             {translate('header.about')}
                         </CNavLink>
+                    </CNavItem>
+                    <CNavItem className='d-lg-none'>
+                        <CDropdown variant="nav-item" popper={false}>
+                            <CDropdownToggle color="secondary">{getCurrentPageName(translate, location)}</CDropdownToggle>
+                            <CDropdownMenu>
+                                <CDropdownItem component={NavLink} to="/">{translate('header.home')}</CDropdownItem>
+                                <CDropdownItem component={NavLink} to="/about">{translate('header.about')}</CDropdownItem>
+                            </CDropdownMenu>
+                        </CDropdown>
                     </CNavItem>
                 </CHeaderNav>
                 <CHeaderNav>
@@ -41,6 +56,13 @@ const AppHeader = (props) => {
             </CContainer>
         </CHeader>
     )
+}
+
+function getCurrentPageName(translate, location){
+    if (location.pathname === '/about') {
+        return translate('header.about');
+    }
+    return translate('header.home')
 }
 
 export default AppHeader;
