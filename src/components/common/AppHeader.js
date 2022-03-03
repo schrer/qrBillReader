7c98651ac745222be7f8b0557b97cc203e361a37
Martin Schröder <ma.schroeder@outlook.com>
2022-03-03 16:41:12 +0100

    Add collapsible for header on small screens

--- src/components/common/AppHeader.js
+++ src/components/common/AppHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import {
     CContainer,
@@ -6,72 +6,77 @@ import {
     CHeaderNav,
     CNavLink,
     CNavItem,
-    CDropdownToggle,
-    CDropdown,
-    CDropdownMenu,
-    CDropdownItem
+    CHeaderToggler,
+    CCollapse,
+    CRow,
+    CNavbar,
+    CButton
 } from '@coreui/react';
 import { useTranslation } from 'react-i18next';
 import DeleteAllBillsModal from '../billspage/DeleteAllBillsModal';
-import {useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { cilHamburgerMenu } from "@coreui/icons";
+import CIcon from '@coreui/icons-react';
 
 
 const AppHeader = (props) => {
     const translate = useTranslation().t;
-    const location = useLocation();
-    //console.log(location)
+    const [visible, setVisible] = useState(false);
+    const location = useLocation().pathname;
+    const navLinks = [];
 
-    // <CHeaderNav className="d-none d-md-flex me-auto">
+    navLinks.push({ to: "/", text: translate('header.home') });
+    navLinks.push({ to: "/settings", text: translate('header.settings') });
+    navLinks.push({ to: "/about", text: translate('header.about') });
+
     return (
         <CHeader position="sticky" className="mb-4">
+            <CRow className='flex-fill justify-content-center' >
+                <CCollapse className="header-collapse" visible={visible} fluid>
+                    {navLinks?.filter(link => location !== link.to).map(link => 
+                        <CRow>
+                            <CNavItem className='d-flex justify-content-center align-items-center'>
+                                <CButton variant='ghost' color='dark' size='lg' component={NavLink} to={link.to}>{link.text}</CButton>
+                            </CNavItem>
+                        </CRow>
+                    )}
+                </CCollapse>
+            </CRow>
             <CContainer fluid>
-                <CHeaderNav>
-                    <CNavItem className='d-none d-lg-block'>
-                        <CNavLink to="/" component={NavLink}>
-                            {translate('header.home')}
-                        </CNavLink>
-                    </CNavItem>
-                    <CNavItem className='d-none d-lg-block'>
-                        <CNavLink to="/settings" component={NavLink}>
-                            {translate('header.settings')}
-                        </CNavLink>
-                    </CNavItem>
-                    <CNavItem className='d-none d-lg-block'>
-                        <CNavLink to="/about" component={NavLink}>
-                            {translate('header.about')}
-                        </CNavLink>
-                    </CNavItem>
-                    <CNavItem className='d-lg-none'>
-                        <CDropdown variant="nav-item" popper={false}>
-                            <CDropdownToggle color="secondary">{getCurrentPageName(translate, location)}</CDropdownToggle>
-                            <CDropdownMenu>
-                                <CDropdownItem component={NavLink} to="/">{translate('header.home')}</CDropdownItem>
-                                <CDropdownItem component={NavLink} to="/settings">{translate('header.settings')}</CDropdownItem>
-                                <CDropdownItem component={NavLink} to="/about">{translate('header.about')}</CDropdownItem>
-                            </CDropdownMenu>
-                        </CDropdown>
-                    </CNavItem>
-                </CHeaderNav>
-                <CHeaderNav>
-                    { props.showDeleteAll &&
-                        <CNavItem>
+                <CNavbar>
+                    <CHeaderNav>
+                        <CHeaderToggler className='d-lg-none' onClick={() => setVisible(!visible)}>
+                            <CIcon size='lg' icon={cilHamburgerMenu}/>&nbsp;
+                            {}
+                            {getCurrentPageName(location, navLinks)}
+                        </CHeaderToggler>
+                        {navLinks?.map(link =>
+                            <CNavItem className='d-none d-lg-block'>
+                                <CNavLink to={link.to} component={NavLink}>
+                                    {link.text}
+                                </CNavLink>
+                            </CNavItem>
+                        )}
+
+                    </CHeaderNav>
+                </CNavbar>
+                {props.showDeleteAll &&
+                        <div class="end-0">
                             <DeleteAllBillsModal />
-                        </CNavItem>
+                        </div>
                     }
-                </CHeaderNav>
             </CContainer>
         </CHeader>
     )
 }
 
-function getCurrentPageName(translate, location){
-    if (location.pathname === '/about') {
-        return translate('header.about');
+function getCurrentPageName(location, navLinks) {
+    const currentLink = navLinks.find(link => link.to === location);
+    if (!currentLink) {
+        return navLinks.find(link => link.to === "/").text;
     }
-    if (location.pathname === '/settings') {
-        return translate('header.settings');
-    }
-    return translate('header.home')
+
+    return currentLink.text;
 }
 
 export default AppHeader;
