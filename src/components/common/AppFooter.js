5e86ab2d11c8d51b51d0c9df0e09aa6400964669
Martin Schröder <ma.schroeder@outlook.com>
2022-03-03 17:08:29 +0100

    Add imprint link to footer

--- src/components/common/AppFooter.js
+++ src/components/common/AppFooter.js
@@ -1,20 +1,16 @@
 
 import React from 'react';
 import { CFooter } from '@coreui/react';
+import { useTranslation } from 'react-i18next';
 
 const AppFooter = () => {
+    const translate = useTranslation().t;
+
   return (
-    <CFooter>
+    <CFooter className="d-flex justify-content-center">
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
-        </a>
-        <span className="ms-1">&copy; 2021 creativeLabs.</span>
-      </div>
-      <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI for React
+        <a href="/impressum" rel="noopener noreferrer">
+          {translate('page.title.imprint')}
         </a>
       </div>
     </CFooter>
